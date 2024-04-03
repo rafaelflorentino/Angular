64037219f94085715fbb60d39b4dee8f93f5b34b
rafael <enterrafael@gmail.com>
2024-04-03 17:17:12 -0300

    carousel

--- ng-bootstrap/src/app/components/home/home.component.ts
+++ ng-bootstrap/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbdCarouselPause } from '../carousel-pause/carousel-pause.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgbModule],
+  imports: [NgbModule, NgbdCarouselPause],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
